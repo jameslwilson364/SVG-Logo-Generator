6fbbf484000c201703b959fb7a4c5b3554f02f92
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-10-27 16:15:27 -0500

    added if else loop for circle, square and triangle creation

--- index.js
+++ index.js
@@ -52,6 +52,45 @@ function writeFile() {
         console.log(response.logoTextColor);
         console.log(response.logoShape);
         console.log(response.logoShapeColor);
+        if (response.logoShape == 'Circle') {
+            const yourLogo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <circle cx="150" cy="100" r="80" fill="${response.logoShapeColor}" />
+          
+            <text x="150" y="120" font-size="60" text-anchor="middle" fill="${response.logoTextColor}">${response.logoInitials}</text>
+          
+            </svg>`;
+            
+            // writes to the new file
+            fs.writeFile('./examples/SquareLogo.svg', `${yourLogo}`, (err) =>
+            err ? console.error(err) : console.log('README logged!')
+            );
+        } else if (response.logoShape == 'Square') {
+            const yourLogo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+            <rect x="25" y="50" width="200" height="200" fill="${response.logoShapeColor}" />
+          
+            <text x="125" y="150" font-size="60" text-anchor="middle" fill="${response.logoTextColor}">${response.logoInitials}</text>
+          
+          </svg>`;
+            
+            // writes to the new file
+            fs.writeFile('./examples/CircleLogo.svg', `${yourLogo}`, (err) =>
+            err ? console.error(err) : console.log('README logged!')
+            );
+        } else if (response.logoShape == 'Triangle') {
+            const yourLogo = `<svg version="1.1" fill="${response.logoShapeColor}" width="300px" height="200px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> 
+            <path d="M21.9,19.3l-9-15.6c-0.1-0.1-0.2-0.2-0.3-0.3c-0.5-0.3-1.1-0.2-1.4,
+            0.3l-9,15.6C2,19.4,2,19.6,2,19.8c0,0.6,0.4,1,1,1h18c0.2,0,0.3,0,0.5-0.1C22,20.4,22.1,19.8,21.9,19.3z"/>
+            <text x="12.1" y="18" font-size="8" text-anchor="middle" fill="${response.logoTextColor}">${response.logoInitials}</text>
+            </svg>`;
+            
+            // writes to the new file
+            fs.writeFile('./examples/TriangleLogo.svg', `${yourLogo}`, (err) =>
+            err ? console.error(err) : console.log('README logged!')
+            );
+        }
+
         
         // writes to the new file
         // fs.writeFile('READMEGEN.md', `${userName}`, (err) =>
